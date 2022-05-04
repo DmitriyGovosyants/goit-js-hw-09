115fb29be1c2d2755160730a431aa0d03c814902
Yelyzaveta Potekhina <91630215+DmitriyGovosyants@users.noreply.github.com>
2022-05-04 11:09:04 +0300

    features/color-switcher

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+const refs = {
+    startBtn: document.querySelector('[data-start]'),
+    stopBtn: document.querySelector('[data-stop]')
+}
+const COLOR_CHANGE_TIME = 1000;
 
+refs.startBtn.addEventListener('click', onBodyColorChange);
+refs.stopBtn.addEventListener('click', onBodyColorStopChange);
+
+function onBodyColorChange() {
+    changeColorTimerID = setInterval(() => {
+        document.body.style.backgroundColor = getRandomHexColor();
+    }, COLOR_CHANGE_TIME);
+    refs.startBtn.disabled = true;
+    refs.stopBtn.disabled = false;
+}
+
+function onBodyColorStopChange() {
+    clearInterval(changeColorTimerID);
+    refs.startBtn.disabled = false;
+    refs.stopBtn.disabled = true;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
